8df2cba40a52c7af20715ec90c1e13bf80fbd13e
Ben Miller <bsmiller25@gmail.com>
2019-03-18 12:06:16 -0400

    tweak: show course id for survey output

--- talentpy/talentpy.py
+++ talentpy/talentpy.py
@@ -7,7 +7,7 @@ import pdb
 class Talent:
     """TalentLMS client class
     """
-    
+
     def __init__(self,
                  apikey=os.environ.get('apikey'),
                  domain=os.environ.get('domain')):
@@ -17,8 +17,9 @@ class Talent:
 
     def get_users(self):
         """get all users"""
-        url = 'https://{}:@{}.talentlms.com/api/v1/users'.format(self.apikey, self.domain)
-        
+        url = 'https://{}:@{}.talentlms.com/api/v1/users'.format(
+            self.apikey, self.domain)
+
         response = requests.request('GET', url)
 
         users = response.json()
@@ -28,7 +29,7 @@ class Talent:
         """get a specific user"""
         url = 'https://{}:@{}.talentlms.com/api/v1/users/id:{}'\
               .format(self.apikey, self.domain, user_id)
-        
+
         response = requests.request('GET', url)
         if response.status_code != 200:
             raise TalentLMSError(response.json()['error']['message'])
@@ -38,8 +39,9 @@ class Talent:
 
     def get_courses(self):
         """get all courses"""
-        url = 'https://{}:@{}.talentlms.com/api/v1/courses'.format(self.apikey, self.domain)
-        
+        url = 'https://{}:@{}.talentlms.com/api/v1/courses'.format(
+            self.apikey, self.domain)
+
         response = requests.request('GET', url)
 
         courses = response.json()
@@ -49,29 +51,28 @@ class Talent:
         """get a specific course"""
         url = 'https://{}:@{}.talentlms.com/api/v1/courses/id:{}'\
               .format(self.apikey, self.domain, course_id)
-        
+
         response = requests.request('GET', url)
         if response.status_code != 200:
             raise TalentLMSError(response.json()['error']['message'])
-        
+
         course = response.json()
         return(course)
-    
 
     def get_survey_response(self, survey_id, user_id):
         """get survey answers for a given survey and user"""
         url = 'https://{}:@{}.talentlms.com/api/v1/getsurveyanswers/survey_id:{},user_id:{}'\
               .format(self.apikey, self.domain, survey_id, user_id)
-        
+
         response = requests.request("GET", url)
 
         if response.status_code != 200:
             raise TalentLMSError(response.json()['error']['message'])
-        
+
         answers = response.json()
-        
+
         return(answers)
-    
+
     def get_survey_responses(self, survey_id, user_ids=[], verbose=False):
         """get responses to a survey from a list of user ids (default is all registered users)"""
 
@@ -82,31 +83,33 @@ class Talent:
         for user_id in user_ids:
             if verbose:
                 print('Getting answers from user: {}'.format(user_id))
-                
+
             try:
                 answer = self.get_survey_response(survey_id, user_id)
             except TalentLMSError:
-                continue                
+                continue
 
             answers.append(answer)
-            
+
         return(answers)
 
     def get_many_survey_responses(self, course_ids=[], verbose=False):
-        """get responses to surveys from multiple courses""" 
+        """get responses to surveys from multiple courses"""
 
         if not course_ids:
             course_ids = [i['id'] for i in self.get_courses()]
-        
-        answers_all = []
+
+        answers_all = {}
         for course_id in course_ids:
             course = self.get_course(course_id)
-            
+
             # get course roster
-            users = [i['id'] for i in course['users'] if i['role'] == 'learner']
+            users = [i['id']
+                     for i in course['users'] if i['role'] == 'learner']
 
             # get course surveys
-            surveys = [i['id'] for i in course['units'] if i['type'] == 'Survey']
+            surveys = [i['id']
+                       for i in course['units'] if i['type'] == 'Survey']
 
             # if there are no surveys, skip this course
             if not surveys:
@@ -116,16 +119,17 @@ class Talent:
                 if verbose:
                     print('Getting results for survey: {}'.format(survey))
                 answers = self.get_survey_responses(survey, users, verbose)
-                answers_all.append(answers)
+                answers_all[course_id] = answers
 
         return(answers_all)
 
     def get_branches(self):
         """get all branches available"""
-        url = 'https://{}:@{}.talentlms.com/api/v1/branches'.format(self.apikey, self.domain)
+        url = 'https://{}:@{}.talentlms.com/api/v1/branches'.format(
+            self.apikey, self.domain)
 
         response = requests.request('GET', url)
-        
+
         branches = response.json()
         return(branches)
 
@@ -137,13 +141,14 @@ class Talent:
         response = requests.request('GET', url)
         if response.status_code != 200:
             raise TalentLMSError(response.json()['error']['message'])
-        
+
         branch = response.json()
         return(branch)
 
     def get_statuses_df(self, course_ids):
         """get user statuses for each course"""
-        statuses = pd.DataFrame(columns=['course_id', 'course_name', 'user_id', 'perc_comp'])
+        statuses = pd.DataFrame(
+            columns=['course_id', 'course_name', 'user_id', 'perc_comp'])
         for cid in course_ids:
             course = self.get_course(cid)
             for user in course['users']:
@@ -152,12 +157,11 @@ class Talent:
                            'course_name': course['name'],
                            'user_id': user['id'],
                            'perc_comp': user['completion_percentage'],
-                    }
+                           }
                     statuses = statuses.append(row, ignore_index=True)
         return(statuses)
-    
+
 
 class TalentLMSError(Exception):
     def __init__(self, error):
         Exception.__init__(self, error)
-
